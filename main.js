a205a4f33fc7fed506379810d64e328a3a9d675e
unknown <krisshonero@gmail.com>
2024-10-03 01:24:04 -0300

    anade token autenticacion

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const Turn = require('node-turn');
+/*const Turn = require('node-turn');
 
 const server = new Turn({
   // El puerto en el que el servidor TURN escuchará las solicitudes
@@ -11,6 +11,25 @@ const server = new Turn({
     username: 'username',
     credential: 'password'
   },
+});
+
+server.start();*/
+
+const Turn = require('node-turn');
+const jwt = require('jsonwebtoken');
+
+// Clave secreta compartida para generar y verificar los tokens
+const sharedSecret = 'my-secret-key';
+
+const server = new Turn({
+  listeningPort: 3478,
+  listeningIps: ['104.131.181.40'], // Escucha en todas las interfaces
+  relayIps: ['104.131.181.40'],     // IP para el tráfico relay
+  authMech: 'hmac',
+  credentials: {
+    'username': 'password'  // Usuario y contraseña fijos
+  },
+  secret: sharedSecret, // Establece el secreto compartido para tokens HMAC
 });
 
 server.start();
